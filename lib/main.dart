8fe9a7b36c98c627bfc374dc4ec16df058514e8c
test <vetal00y@gmail.com>
2023-09-12 16:45:16 +0300

    firebase init

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:podcasts_ua_app/core/podcasts_ua_app.dart';
+import 'package:podcasts_ua_app/firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const PodcastsUaApp());
 }
 
